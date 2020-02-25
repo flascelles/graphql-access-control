53612ca1dcb562ad3f6be355a64160372a791e13
Francois Lascelles <francoislascelles@pingidentity.com>
2020-02-25 12:46:17 -0800

    Implement token introspection with Ping Federate and async GraphQL resolvers

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
 # GraphQL Access Control banking sample
-Example Apollo GraphQL service with sample open banking data which implements access control rules at the resolver level by reading a requester subject dereferenced from an incoming OAuth token and populated into the ApolloServer context.
+Example Apollo GraphQL service with sample open banking data which implements access control rules at the resolver level by reading a requester subject de-referenced from an incoming OAuth token and populated into the ApolloServer context.
 
-This illustrates basic access control in which the banking accounts returned by the GraphQL queries are only the ones that are associated with the requester of the query as identified by the incoming token attached to the query.
+This illustrates basic access control in which the banking accounts returned by the GraphQL queries are only the ones that are associated with the identity of the requester of the query as identified by the incoming OAuth token attached to the query. Skip to the bottom of this readme for instructions on how to run this locally.
 
-Adding the requester subject into the context is done as part of the Apollo server instantiation (see `index.js`):
+In order to get to the requester subject, the `express.Request` is first added to the `ApolloServer` context (see `index.js`):
  ```
+// instantiate Apollo express server with a hook to incorporate incoming request inside the transaction context
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    // try to retrieve a user with the token
-    const subject = reqToSubject(req);
-    // add the user to the context
-    return { subject };
+    // add the request to the context
+    const request = req;
+    return {request};
   },
 });
-
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
 ```
-The banking data is populated with object that have an _owner_ attribute that can be used to match the ownership of the bank account with the subject of the requester (see `data.js`):
+The hardcoded banking data is populated with object that have an _owner_ attribute that can be used to match the ownership of the bank account with the subject of the requester (see `data.js`):
 ```
 exports.accounts = [
     {
@@ -44,12 +40,14 @@ exports.accounts = [
 ...
 ];
 ```
- 
-The enforcement of the access control rules is done at the GraphQL resolver level.
+
+The GraphQL resolver implements both the de-referencing of the subject from the incoming token (via the request in the context) as well as the matching of which elements from the data should be returned based on the identity of the requester.
+Because the de-referencing of the subject can involve an outbound call to an OAuth token introspection endpoint, the GraphQL resolver is implemented in async mode and you need to `await` on the method which looks up the subject from the token (a Node.JS quirk I'm still getting used to as a Java developer).
 ```
 const resolvers = {
   Query: {
-    accounts: (root, args, context) => {
+    accounts: async (root, args, context) => {
+      await contextToSubject(context);
       console.log("returning accounts for user " + context.subject);
       // iterate across accounts, only return matching for that user
       let i;
@@ -57,13 +55,15 @@ const resolvers = {
       for (i = 0; i < data.accounts.length; i++) {
         // add to output only if owner matches
         // this statement is key to the authorization decision illustrated in this example
-        if (data.accounts[i].owner.localeCompare(context.subject)) {
+        if (data.accounts[i].owner.localeCompare(context.subject) == 0) {
           output[output.length] = data.accounts[i];
         }
       }
+      console.log("" + output.length + " items");
       return output;
     },
-    transfers: (root, args, context) => {
+    transfers: async (root, args, context) => {
+      await contextToSubject(context);
       console.log("returning transfers for user " + context.subject);
       // iterate across transfers, only return matching for that user
       let i;
@@ -71,16 +71,93 @@ const resolvers = {
       for (i = 0; i < data.transfers.length; i++) {
         // add to output only if owner of creditor account matches
         // this statement is key to the authorization decision illustrated in this example
-        if (data.transfers[i].creditor.owner.localeCompare(context.subject)) {
+        if (data.transfers[i].creditor.owner.localeCompare(context.subject) == 0) {
           output2[output2.length] = data.transfers[i];
         }
       }
+      console.log("" + output2.length + " items");
       return output2;
     },
   },
 };
 ```
+Two separate identity subject de-referencing methods are implemented. You can uncomment the one you want to use or add your own. The first de-referencing method makes an outbound call to a token introspection endpoint on Ping Federate to retrieve attributes associated with the incoming token. The second alternative parses fake tokens and retrieve a subject from it. This alternative is provided for cases where you want to avoid any dependencies (such as a token service) to just work on the GraphQL implementation.
+```
+// ==== TOKEN INTROSPECTION ====
+// Introspection for incoming tokens with Ping Federate's OAuth token introspection endpoint
+// This is kept simple, and the call out is made out each time, an obvious optimization would
+// be to cache introspection results to avoid calling each time
+// the Ping Federate oauth introspection endpoint typically returns one of the following two responses:
+// 1.                      {
+//                           "Username" : "469863216813@example.com",
+//                           "active" : true,
+//                           "OrgName" : "PingIdentity",
+//                           "token_type" : "Bearer",
+//                           "exp" : 1582660570,
+//                           "client_id" : "graphql_client"
+//                         }
+//
+// 2. (in case of failure) {
+//                           "active" : false
+//                         }
+//
+async function tokToSubjectIntrospection(tok) {
+  try {
+    let http_promise = getPingFedPromise(tok);
+    // response_body holds response from server that is passed when Promise is resolved
+    let response_body = await http_promise;
+    const introspection_response = JSON.parse(response_body);
+    if (!introspection_response.active) {
+      console.log("Token introspection: The incoming token is invalid or in not active");
+      // You may want to return a 401 here
+      return "not authenticated";
+    } else if (introspection_response.active) {
+      // isolate subject
+      const username = introspection_response.Username;
+      const subject = username.substring(0, username.indexOf('@'));
+      return subject;
+    } else {
+      console.log("Token introspection: Unexpected token introspection response: " + response_body);
+      return "Introspection failed";
+    }
+  } catch(error) {
+    // Promise rejected
+    console.log(error);
+    return "Introspection failed";
+  }
+}
 
+// SELECT TOKEN MODE HERE
+// This is the function which dereferences the requester subject from the incoming token
+// you could do this many ways such as decode a JWT or call an external userInfo (e.g. PingFederate)
+// endpoint and cache the results from that userInfo endpoint
+async function tokToSubject(tok) {
+  // Two different ways to accomplish this are implemented, uncomment one or the other
+
+  // Alternative 1
+  return await tokToSubjectIntrospection(tok);
+
+  // Alternative 2
+  // return tokToSubjectFakeToks(tok);
+}
+
+// dereference the requester subject from the incoming request
+async function contextToSubject(context) {
+  // looking for token from http header authorization using value pattern "Bearer _token_"
+  const req = context.request;
+  const token = req.headers.authorization || '';
+
+  // Valid token require authorization value to be longer than 7 because it starts with "Bearer "
+  if (token.length > 7) {
+    // isolate the token from the authorization header value
+    let bearertok = token.substring(7);
+    // resolve your user subject from the token
+    context.subject = await tokToSubject(bearertok);
+  } else {
+    context.subject = "no subject present";
+  }
+}
+``` 
 ## Running the GraphQL server sample
 
 Step 1 - Clone repo
--- index.js
+++ index.js
@@ -44,7 +44,8 @@ const data = require('./data.js');
 //
 const resolvers = {
   Query: {
-    accounts: (root, args, context) => {
+    accounts: async (root, args, context) => {
+      await contextToSubject(context);
       console.log("returning accounts for user " + context.subject);
       // iterate across accounts, only return matching for that user
       let i;
@@ -52,13 +53,15 @@ const resolvers = {
       for (i = 0; i < data.accounts.length; i++) {
         // add to output only if owner matches
         // this statement is key to the authorization decision illustrated in this example
-        if (data.accounts[i].owner.localeCompare(context.subject)) {
+        if (data.accounts[i].owner.localeCompare(context.subject) == 0) {
           output[output.length] = data.accounts[i];
         }
       }
+      console.log("" + output.length + " items");
       return output;
     },
-    transfers: (root, args, context) => {
+    transfers: async (root, args, context) => {
+      await contextToSubject(context);
       console.log("returning transfers for user " + context.subject);
       // iterate across transfers, only return matching for that user
       let i;
@@ -66,71 +69,162 @@ const resolvers = {
       for (i = 0; i < data.transfers.length; i++) {
         // add to output only if owner of creditor account matches
         // this statement is key to the authorization decision illustrated in this example
-        if (data.transfers[i].creditor.owner.localeCompare(context.subject)) {
+        if (data.transfers[i].creditor.owner.localeCompare(context.subject) == 0) {
           output2[output2.length] = data.transfers[i];
         }
       }
+      console.log("" + output2.length + " items");
       return output2;
     },
   },
 };
 
+// fake token processing for when you don't want to interact with a real token service to
+// keep dependencies at a minimum when working on isolated GraphQL code
+// fake tokens are just base64 objects that have a .subject property
+function tokToSubjectFakeToks(tok) {
+  let decoded = Buffer.from(tok, 'base64').toString();
+  try {
+    let ojedTok = JSON.parse(decoded);
+    return ojedTok.subject;
+  } catch (error) {
+    console.log("error while parsing fake token, maybe you are passing a real token instead? " + error);
+    return "Fake token failure";
+  }
+}
+
+// https call to the token service (Ping Federate) token introspection endpoint
+// this gets a response back from the token introspection endpoint to decide if
+// the token is valid and to get attributes associated with it
+function getPingFedPromise(tok) {
+  // a real world implementation would not include a client id and hardcoded basic auth token in the code
+  // this is just for illustration purposes
+  const client_id = 'graphql_client';
+  const https = require('https');
+  const data = 'token=' + tok + '&client_id=' + client_id;
+  const options = {
+    hostname: 'localhost',
+    port: 9031,
+    path: '/as/introspect.oauth2',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic Z3JhcGhxbF9jbGllbnQ6',
+      'Content-Length': data.length
+    }
+  };
+  return new Promise((resolve, reject) => {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    const req = https.request(options, (response) => {
+      let chunks_of_data = [];
+      response.on('data', (fragments) => {
+        chunks_of_data.push(fragments);
+      });
+      response.on('end', () => {
+        let response_body = Buffer.concat(chunks_of_data);
+        resolve(response_body.toString());
+      });
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+    req.write(data);
+    req.end();
+  });
+}
+
+// ==== TOKEN INTROSPECTION ====
+// Introspection for incoming tokens with Ping Federate's OAuth token introspection endpoint
+// This is kept simple, and the call out is made out each time, an obvious optimization would
+// be to cache introspection results to avoid calling each time
+// the Ping Federate oauth introspection endpoint typically returns one of the following two responses:
+// 1.                      {
+//                           "Username" : "469863216813@example.com",
+//                           "active" : true,
+//                           "OrgName" : "PingIdentity",
+//                           "token_type" : "Bearer",
+//                           "exp" : 1582660570,
+//                           "client_id" : "graphql_client"
+//                         }
 //
-// this is the function which dereferences the requester subject from the incoming token
+// 2. (in case of failure) {
+//                           "active" : false
+//                         }
+//
+async function tokToSubjectIntrospection(tok) {
+  try {
+    let http_promise = getPingFedPromise(tok);
+    // response_body holds response from server that is passed when Promise is resolved
+    let response_body = await http_promise;
+    const introspection_response = JSON.parse(response_body);
+    if (!introspection_response.active) {
+      console.log("Token introspection: The incoming token is invalid or in not active");
+      // You may want to return a 401 here
+      return "not authenticated";
+    } else if (introspection_response.active) {
+      // isolate subject
+      const username = introspection_response.Username;
+      const subject = username.substring(0, username.indexOf('@'));
+      return subject;
+    } else {
+      console.log("Token introspection: Unexpected token introspection response: " + response_body);
+      return "Introspection failed";
+    }
+  } catch(error) {
+    // Promise rejected
+    console.log(error);
+    return "Introspection failed";
+  }
+}
+
+// SELECT TOKEN MODE HERE
+// This is the function which dereferences the requester subject from the incoming token
 // you could do this many ways such as decode a JWT or call an external userInfo (e.g. PingFederate)
 // endpoint and cache the results from that userInfo endpoint
-//
-function tokToSubject(tok) {
-  // Two different ways to accomplish this are suggested, uncomment one or the other
+async function tokToSubject(tok) {
+  // Two different ways to accomplish this are implemented, uncomment one or the other
 
   // Alternative 1
-  // in this version, a userinfo endpoint on PingFederate is called to retrieve attributes assocaited with
+  // in this version, a token introspection endpoint on PingFederate is called to retrieve attributes associated with
   // the token
-  // todo
+  // return await tokToSubjectIntrospection(tok);
 
   // Alternative 2
   // in this alternate version of the code, the tokens are just a base64 encoded javascript object (JSON)
   // see data.js for more info on how to use these tokens in this example
   // this option is made available in order to minimize dependencies with this sample such as external token
   // servers, identity stores, etc
-  let decoded = Buffer.from(tok, 'base64').toString();
-  console.log("decoded " + decoded);
-  let ojedTok = JSON.parse(decoded);
-  return ojedTok.subject;
+  return tokToSubjectFakeToks(tok);
 }
 
-//
 // dereference the requester subject from the incoming request
-//
-function reqToSubject(req) {
+async function contextToSubject(context) {
   // looking for token from http header authorization using value pattern "Bearer _token_"
+  const req = context.request;
   const token = req.headers.authorization || '';
 
   // Valid token require authorization value to be longer than 7 because it starts with "Bearer "
   if (token.length > 7) {
     // isolate the token from the authorization header value
     let bearertok = token.substring(7);
-    console.log("incoming token= " + bearertok);
     // resolve your user subject from the token
-    return tokToSubject(bearertok);
+    context.subject = await tokToSubject(bearertok);
   } else {
-    return "anonymous";
+    context.subject = "no subject present";
   }
 }
 
-// instantiate Apollo express server with a hook to incorporate a requester subject inside the transaction context
+// instantiate Apollo express server with a hook to incorporate incoming request inside the transaction context
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    // try to retrieve a user with the token
-    const subject = reqToSubject(req);
-    // add the user to the context
-    return { subject };
+    // add the request to the context
+    const request = req;
+    return {request};
   },
 });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
-
